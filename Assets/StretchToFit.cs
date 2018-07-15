a73070943bb7f699fcd469ee5344b51188ee406c
Changbai Li <lcb931023@gmail.com>
2018-07-15 17:58:07 +0800

    Initial Commit: add strand test, kinect package, and kinect example

---
+++ Assets/StretchToFit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StretchToFit : MonoBehaviour {
+
+	public Transform head;
+	public Transform tail;
+	// Record intial scale
+	private Vector3 scale0;
+
+	// Use this for initialization
+	void Start () {
+		scale0 = transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pH = head.position;
+		Vector3 pT = tail.position;
+		// position in middle of head and tail
+		transform.position = (pH + pT) / 2;
+		// rotate to tail direction
+		transform.LookAt(pT);
+		// stretch length
+		Vector3 scale = scale0;
+		scale.z = scale0.z * Vector3.Distance(pH, pT);
+		transform.localScale = scale;
+	}
+}
